bf6b6be27f4b1c0591be14bbe1ecdbf899e3159f
Rinat <rtainov@noon.com>
2017-05-16 17:18:09 +0400

    added caching for external service

--- app/controllers/FlightsController.scala
+++ app/controllers/FlightsController.scala
@@ -3,14 +3,20 @@ package controllers
 import javax.inject._
 
 import play.api._
-import play.api.libs.json.{JsArray, Json, Writes}
+import play.api.libs.json.{Json, Writes}
 import play.api.libs.ws.WSClient
 import play.api.mvc._
 import play.api.Logger
+import play.api.cache.CacheApi
 
-class FlightsController @Inject()(ws: WSClient) extends Controller {
+import scala.concurrent.{Future, Promise}
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
+class FlightsController @Inject()(ws: WSClient, cache: CacheApi) extends Controller {
 
   val ROUTES_URL = "https://api.ryanair.com/core/3/routes/"
+  val ROUTES_CACHE = "ryanair.routes"
 
   implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext
 
@@ -44,21 +50,36 @@ class FlightsController @Inject()(ws: WSClient) extends Controller {
 
   def interconnections(departure: String, arrival: String) = Action.async { implicit request =>
     Logger.debug(s"departure: $departure, arrival: $arrival")
-    val routesRequest = ws.url(ROUTES_URL)
-    routesRequest.get().map {
-      case response if response.status == OK => {
-        val connections = response.json.as[Seq[Connection]]
-        val directFligths = connections.filter(c => c.airportFrom == departure && c.airportTo == arrival)
-        val oneConnectionFlights = for {
-          c1 <- connections
-          if c1.airportTo == arrival
-          c2 <- connections
-          if c2.airportFrom == departure && c2.airportTo == c1.airportFrom
-        } yield Seq(c1, c2)
-        val result = Seq(FlightTrip(0, directFligths)) ++ oneConnectionFlights.map(e => FlightTrip(1, e))
-        Ok(Json.toJson(result))
+    getConnections.map(e => Ok(Json.toJson(calculateFlightTrips(e, departure, arrival)))).recover {
+      case e =>
+        Logger.debug("Failed to retrieve connections", e)
+        InternalServerError
+    }
+  }
+
+  private def calculateFlightTrips(connections: Seq[Connection], departure: String, arrival: String): Seq[FlightTrip] = {
+    val directFligths = connections.filter(c => c.airportFrom == departure && c.airportTo == arrival)
+    val oneConnectionFlights = for {
+      c1 <- connections
+      if c1.airportTo == arrival
+      c2 <- connections
+      if c2.airportFrom == departure && c2.airportTo == c1.airportFrom
+    } yield Seq(c1, c2)
+    Seq(FlightTrip(0, directFligths)) ++ oneConnectionFlights.map(e => FlightTrip(1, e))
+  }
+
+  private def getConnections: Future[Seq[Connection]] = cache.get[Seq[Connection]](ROUTES_CACHE) match {
+    case Some(conns) => Future(conns)
+    case None => {
+      val routesRequest = ws.url(ROUTES_URL)
+      routesRequest.get().map {
+        case response if response.status == OK => {
+          val connections = response.json.as[Seq[Connection]]
+          cache.set(ROUTES_CACHE, connections, 1 day)
+          connections
+        }
+        case response => throw new Exception(s"RyanAir routes service failed with error code: ${response.status}")
       }
-      case _ => InternalServerError
     }
   }
 
